fece40165e0a86134e0b479e87ef6da79d3a9130
Dev Earl <113250003+EarlDes@users.noreply.github.com>
2023-07-07 13:17:05 +0800

    Add files via upload

---
+++ beshy.js
@@ -0,0 +1,53 @@
+function beshy() {
+    var inputText = document.getElementById("inputTxt").value;
+    var beshyText = inputText.split(" ");
+    var output = "";
+    
+    for (var i = 0; i < beshyText.length; i++) {
+        output += beshyText[i] + " 🤸🏼‍♂️ ";
+    }
+    
+    document.getElementById("outputTxt").innerText = output;
+}
+
+function play() {
+    var audio = new Audio("audio.mp3");
+    audio.play();
+}
+
+function copyText() {
+    var outputText = document.getElementById("outputTxt");
+    var textRange = document.createRange();
+    textRange.selectNode(outputText);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(textRange);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    
+    var responseMsg = document.getElementById("responseMsg");
+    responseMsg.innerText = "Copy successful!";
+    setTimeout(function() {
+        responseMsg.innerText = "";
+    }, 2000); // Clear the response message after 2 seconds
+}
+
+var emojiAnimation = document.querySelector('.emoji-animation');
+
+var numEmoji = 30;
+
+for (var i = 0; i < numEmoji; i++) {
+    var img = document.createElement('img');
+    img.src = 'tumbling-emoji.png';
+    img.alt = 'Tumbling Emoji';
+    img.classList.add('tumble-animation');
+    img.style.top = Math.random() * 100 + '%';
+    img.style.left = Math.random() * 100 + '%';
+    emojiAnimation.appendChild(img);
+}
+
+var backgroundMusic = new Audio("audio.mp3"); // Replace "background_music.mp3" with your background music file source
+backgroundMusic.loop = true;
+
+document.addEventListener("DOMContentLoaded", function() {
+  backgroundMusic.play();
+});
